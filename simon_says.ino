615f92ff6d84a8105c1d70c08f8076671b8384dc
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-01-19 16:10:14 +0100

    Some update that were not pushed

--- simon_says.ino
+++ simon_says.ino
@@ -4,15 +4,6 @@
 #define BUZZER 8
 #define DEBUG false
 
-enum state {
-  SHOW_NOTES, 
-  WRONG_ORDER,
-  BUTTON_1_PRESSED,
-  BUTTON_2_PRESSED,
-  BUTTON_3_PRESSED,
-  WIN
-} state_var = SHOW_NOTES;
-
 void setupElement(int number, int offset, int type) {
   for (int i = 0; i < number; i++) {
     pinMode(i+offset, type);
@@ -32,17 +23,30 @@ void setup()
 
 void loop()
 {
-  int notes[3] = {1,2,3}; // TODO First dummy order, then need to randomize it.  
+  static enum state {
+    SHOW_NOTES, 
+    WAITING_INPUT,
+    WRONG_ORDER,
+    BUTTON_1_PRESSED,
+    BUTTON_2_PRESSED,
+    BUTTON_3_PRESSED,
+    WIN
+  } state_var = SHOW_NOTES;
+  int notes[] = {2,3,2,3,2,3,2,3}; // TODO First dummy order, then need to randomize it.  
 
   switch (state_var) {
     case SHOW_NOTES:
+      state_var = WAITING_INPUT;
       // TODO Show notes in order & buzzer
+      showNotes(notes);
       break;
      case WRONG_ORDER:
         // Afficher toutes les leds à 1 pendant 5s
       break;
      case WIN:
         // Afficher toutes les leds pendant 10s
+       break;
+     case WAITING_INPUT:
        break;
      case BUTTON_1_PRESSED:
       // TODO Gérer le bouton 1 dans une fonction générique
@@ -57,3 +61,20 @@ void loop()
 
   // TODO Si bouton 1, 2  ou 3 pressé et que state_var != SHOW_NOTE, WIN, WRONG_ORDER, set state_var à BUTTON_N_PRESSED 
 }
+
+
+void showNotes(int notes[]) {
+  for (int i = 0; i <= sizeof(notes); i++) {
+    if (DEBUG) {
+      Serial.println(notes[i]);
+    }
+    turnItOnAndOff(notes[i], 100);
+  }
+}
+
+void turnItOnAndOff(int element, int timer) {
+  digitalWrite(element, HIGH);
+  delay(timer);
+  digitalWrite(element, LOW);
+}
+
